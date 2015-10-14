75219e3f84f1b74880812cd46df38483e4604459
crastr <afoninalexeym@gmail.com>
2015-10-14 22:24:08 +0300

    Update task0.py

--- homeworks/Homework2/task0.py
+++ homeworks/Homework2/task0.py
@@ -1,18 +1,19 @@
 __author__ = 'Alex'
-def plural (x, *args):
+
+
+def plural(x, args):
     if x % 10 == 1 and x % 100 != 11:
         return args[0]
     elif (1 < x % 10 < 5) and (14 < x % 100 or x % 100 < 12):
         return args[1]
     else:
         return args[2]
+
+
+vocab = {'утюг': ['утюг', 'утюга', 'утюгов'],
+         'ложка': ['ложка', 'ложки', 'ложек'],
+         'гармошка': ['гармошка', 'гармошки', 'гармошек'],
+         'чайник': ['чайник', 'чайника', 'чайников']}
 word = input()
 number = int(input())
-if word == 'утюг':
-    print("%d %s" % (number, plural(number, 'утюг', 'утюга', 'утюгов')))
-if word == 'ложка':
-    print("%d %s" % (number, plural(number, 'ложка', 'ложки', 'ложек')))
-if word == 'гармошка':
-    print("%d %s" % (number, plural(number, 'гармошка', 'гармошки', 'гармошек')))
-if word == 'чайник':
-    print("%d %s" % (number, plural(number, 'чайник', 'чайника', 'чайников')))
+print("%d %s" % (number, plural(number, vocab[word])))
