5c11fa98a05e4eae42e794874df6de5ca680b6b6
crastr <afoninalexeym@gmail.com>
2015-10-14 22:19:49 +0300

    Update task4.py
    once again

--- homeworks/Homework2/task4.py
+++ homeworks/Homework2/task4.py
@@ -1,17 +1,23 @@
 __author__ = 'Alex'
+
+
 def euclid(n, m):
     if n % m == 0:
         return m
     else:
         return euclid(m, n % m)
-def rpfilter (number, args):
+
+
+def rpfilter(number, args):
     nondividers = []
     for arg in args:
         if euclid(number, arg) == 1:
             nondividers.append(str(arg))
-    return ' '.join(nondividers)
+    return nondividers
+
+
 a = ([int(i) for i in input().split()])
-if len((rpfilter(a[0], list(a[1::])))) == 0:
+if len(' '.join(rpfilter(a[0], list(a[1::])))) == 0:
     print('None')
 else:
-    print(rpfilter(a[0], list(a[1::])))
+    print(' '.join(rpfilter(a[0], list(a[1::]))))
