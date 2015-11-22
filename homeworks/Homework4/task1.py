86380bcbf785cad4b11a60e58da4de7d624ce202
crastr <afoninalexeym@gmail.com>
2015-11-22 21:15:49 +0300

    Update task1.py

--- homeworks/Homework4/task1.py
+++ homeworks/Homework4/task1.py
@@ -3,7 +3,7 @@ with open("yazkora.txt", "r") as f:
     text = f.read()
 sentences = text.replace('\n', ' ')
 sentences = sentences.split('.')
-answer = [' ']
+answer = ['']
 for line in sentences:
     words = line.split(' ')
     for word in words:
@@ -11,7 +11,7 @@ for line in sentences:
             answer.append(word)
     answer.append('\n')
 with open("answer.txt", "w") as n:
-    n.write(' '.join(answer))
+    n.write(''.join(answer))
 
 
 
